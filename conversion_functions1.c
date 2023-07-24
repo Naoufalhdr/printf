c48f422d5def1f464d817d90a4f562b540f4b87d
Naoufalhdr <naoufalhdr96@gmail.com>
2023-07-24 07:07:00 -0700

    fixing some betty style and changin the argument 'b' to unsigned int type

--- conversion_functions1.c
+++ conversion_functions1.c
@@ -1,25 +1,34 @@
 #include "main.h"
 
-char *convert_binary(va_list b)
+/**
+ * convert_to_binary - convert an unsigned integer to binary.
+ * @b: a variable argument list containing an unsigned int to be converted.
+ *
+ * Return: a pointer to string containing the binary representation of
+ *         the unsigned integer.
+ */
+char *convert_to_binary(va_list b)
 {
-	int num, bit_value;
+	unsigned int num, bit_value;
 	int i, j, k;
-	int bits[64];
+	int bits[32];
 	char *bits_str;
 
-	num = va_arg(b, int);
+	num = va_arg(b, unsigned int);
 
+	/* Converting the number to binary */
 	for (i = 0; num != 0; i++)
 	{
 		bit_value = num % 2;
 		bits[i] = bit_value;
 		num /= 2;
 	}
-
+	/* Allocating memory for the binary string based on the number of bits*/
 	bits_str = malloc((i + 1) * sizeof(char));
 	if (bits_str == NULL)
 		return (NULL);
 
+	/* Copying the elements in the bits[32] into the bits_str string */
 	for (j = i - 1, k = 0; j >= 0; j--, k++)
 		bits_str[k] = bits[j] + '0';
 	bits_str[k] = '\0';
--- utility_functions.c
+++ utility_functions.c
@@ -40,7 +40,7 @@ char *(*get_conversion_specifier(char s))(va_list)
 		{"d", convert_decimal},
 		{"i", convert_decimal},
 		{"u", convert_unsigned},
-		{"b", convert_binary},
+		{"b", convert_to_binary},
 		{NULL, NULL}
 	};
 
