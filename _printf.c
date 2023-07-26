515980265150524604ebaf6e69894ca19aa9f511
Naoufalhdr <naoufalhdr96@gmail.com>
2023-07-26 10:06:49 -0700

    Add the field width modifier and '0' flag character for non custom conversion specifier

--- _printf.c
+++ _printf.c
@@ -8,13 +8,14 @@
  *       in the format string.
  * Return: The number of characters printed (excluding the null byte).
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count, i, j, flag;
+	int count, i, j, k, width_flag, holder_len;
+	int plus_flag = 0, space_flag = 0, hash_flag = 0, zero_flag = 0;
 	char *holder;
 	char *(*conversion_func)(va_list);
+	int width = 0;
 
 	va_start(args, format);
 	count = 0;
@@ -23,43 +24,77 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			while (format[i + 1] == '+' || format[i + 1] == ' ' || format[i + 1] == '#')
+			while (format[i + 1] == '+' || format[i + 1] == ' ' || format[i + 1] == '#' || format[i + 1] == '0')
 			{
-				if (format[i + 1] == '+')
-					flag = 1;
-				if (format[i + 1] == ' ')
-					flag = 2;
-				if (format[i + 1] == '#')
-					flag = 3;
+				if (format[i + 1] == '+')	
+					plus_flag = 1;
+				else if (format[i + 1] == ' ')
+					space_flag = 2;
+				else if (format[i + 1] == '#')
+					hash_flag = 3;
+				else if (format[i + 1] == '0')
+					zero_flag = 4;
 				i++;
+			}
+			if (format[i + 1] >= '1' && format[i + 1] <= '9')
+			{
+				width_flag = 1;
+				width = format[i + 1] - '0';
+				i++;
+				while (format[i + 1] >= '0' && format[i + 1] <= '9')
+				{
+					width = width * 10 + (format[i + 1] - '0');
+					i++;
+				}
 			}
 			conversion_func = get_conversion_specifier(format[i + 1]);
 			if (conversion_func)
 			{
 				holder = conversion_func(args);
+				holder_len = _strlen(holder);
 				j = 0;
-				while (holder[j])
+				while (holder[j] != '\0')
 				{
-					if (holder[0] != '-' && j == 0 && flag == 1 && (format[i + 1] == 'd' || format[i + 1] == 'i'))
+					if (holder[0] != '-' && j == 0 && plus_flag && (format[i + 1] == 'd' || format[i + 1] == 'i'))
+					{
 						_putchar('+');
-					if (holder[0] != '-' && j == 0 && flag == 2)
+						holder_len++;
+					}
+					if (holder[0] != '-' && j == 0 && space_flag && (format[i + 1] == 'd' || format[i + 1] == 'i'))
+					{
 						_putchar(' ');
-					if (holder[0] != '0' && flag == 3 && format[i + 1] == 'o')
+						holder_len++;
+					}
+					if (holder[0] != '0' && hash_flag && format[i + 1] == 'o')
 						_putchar('0');
-					if (holder[0] != '0' && flag == 3 && format[i + 1] == 'x')
+					if (holder[0] != '0' && hash_flag && format[i + 1] == 'x')
 					{
 						_putchar('0');
 						_putchar('x');
 					}
-					if (holder[0] != '0' && flag == 3 && format[i + 1] == 'X')
+					if (holder[0] != '0' && hash_flag && format[i + 1] == 'X')
 					{
 						_putchar('0');
 						_putchar('X');
 					}
+					if (width_flag == 1 && width > holder_len)
+					{
+							for (k = 0; k < width - holder_len; k++)
+							{
+								if (zero_flag == 4)
+									_putchar('0');
+								else
+									_putchar(' ');
+							}
+					}
 					_putchar(holder[j]);
 					j++;
 					count++;
-					flag = 0;
+					plus_flag = 0;
+					space_flag = 0;
+					hash_flag = 0;
+					zero_flag = 0;
+					width_flag = 0;
 				}
 				i++;
 			}
@@ -71,10 +106,7 @@ int _printf(const char *format, ...)
 			_putchar(format[i]);
 			count++;
 		}
-
 	}
-
 	va_end(args);
 	return (count);
 }
-
